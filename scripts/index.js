be56b97cac672e3355ef326a50ca72f1f519a47d
JoycesCodes <jjyoung145@gmail.com>
2021-05-30 16:28:16 -0400

    added basic structure to html and data to js

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let cityStates = [{
+    state: "California",
+    stateAbbr: "CA",
+    cities: ["Los Angeles", "San Francisco", "San Diego"]
+ },
+ {
+    state: "Colorado",
+    stateAbbr: "CO",
+    cities: ["Aspen", "Boulder", "Denver", "Pagosa Springs"]
+ },
+ {
+    state: "Texas",
+    stateAbbr: "TX",
+    cities: ["Austin", "Dallas", "Houston", "San Antonio"]
+ }
+];
